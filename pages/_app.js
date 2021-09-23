509dd831b50ff9bb36c15d1f931dbdeb0a78a3d1
Alex <alexbilozir@gmail.com>
2021-09-23 18:22:00 +0300

    upd

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/style2.css'
+import Head from "next/head";
 // import '../styles/normalize.css'
 // import '../styles/webflow.css'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <>
+    <Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png"/>
+ <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png"/>
+ <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"/>
+   </Head>
+  <Component {...pageProps} />
+  </>
+  );
 }
 
 export default MyApp
