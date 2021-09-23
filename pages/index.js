4ced4327ab5cab3b53efec895353a59a9d6e5a69
Alex <alexbilozir@gmail.com>
2021-09-23 18:06:22 +0300

    upd

--- pages/index.js
+++ pages/index.js
@@ -110,7 +110,9 @@ export default function Home() {
 <html>
 	<head>
 		<title>RGB Punks</title>
-		<link rel="icon" href="/images/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 
 		<meta property="og:title" content="RGB Punks" key="ogtitle" />
 		<meta property="og:description" content=" !!!!!!!!" key="ogdesc" />
